237b88b80e7a172659cd5c45e14e7e48aa138c63
eagobert <eagobert@hotmail.com>
2018-05-16 16:01:49 -0500

    update taskView.js

--- src/scripts/DOMManager/taskView.js
+++ src/scripts/DOMManager/taskView.js
@@ -5,6 +5,15 @@ const taskManager = require("../APIManager/tasksManager")
 
 const taskList = function(){
 
+    //Get the element with the id "tasks"
+    const parentEl = document.getElementById("tasks")
+
+    //Create an unordered list element
+    const ul = document.createElement("ul")
+
+    //Append the ul element to the parent element with the id "tasks"
+    parentEl.appendChild(ul)
+
     //Get all tasks in the database then....
     taskManager.getAllTasks().then(allTasks => {
 
