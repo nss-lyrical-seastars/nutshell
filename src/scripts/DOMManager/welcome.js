b7bcfc443521616be72c0db0e2b28fca15af1993
Jessica Swift <jessica.nicole.swift@gmail.com>
2018-05-17 09:28:54 -0500

    modified welcome file

--- src/scripts/DOMManager/welcome.js
+++ src/scripts/DOMManager/welcome.js
@@ -1,56 +1,56 @@
 const fragment = document.createDocumentFragment();
 
-const welcomePage = function () {
-    // create element
-    const welcomeLogo = document.createElement("h1");
-    // logo H1
-    welcomeLogo.textContent = "Lyrical Seastars Welcome";
-    // append to fragment
-    fragment.appendChild(welcomeLogo)
+const welcomePage = function() {
+// create element
+const welcomeLogo = document.createElement("h1");
+// logo H1
+welcomeLogo.textContent = "Lyrical Seastars Welcome";
+// append to fragment
+fragment.appendChild(welcomeLogo)
 
-    // e-mail input
-    const userEmail = document.createElement("input");
-    userEmail.type = "text"
-    // e-mail placeholder
-    userEmail.placeholder = "E-mail"
-    // append to  div
-    fragment.appendChild(userEmail)
+// e-mail input
+const userEmail = document.createElement("input");
+userEmail.type = "text"
+// e-mail placeholder
+userEmail.placeholder = "E-mail"
+// append to  div
+fragment.appendChild(userEmail)
 
-    // username input
-    const userName = document.createElement("input");
-    userName.type = "text"
-    // username placeholder
-    userName.placeholder = "User Name"
-    // append to  div
-    fragment.appendChild(userName)
+// username input
+const userName = document.createElement("input");
+userName.type = "text"
+// username placeholder
+userName.placeholder = "User Name"
+// append to  div
+fragment.appendChild(userName)
 
-    // submit Button
-    const logIn = document.createElement("button");
-    logIn.type = "button"
-    // Button Text
-    logIn.textContent = "Log In"
-    logIn.addEventListener("click", function (event) {
-        console.log(event);
-    });
+// submit Button
+const logIn = document.createElement("button");
+logIn.type = "button"
+// Button Text
+logIn.textContent = "Log In"
+logIn.addEventListener("click", function (event) {
+    console.log(event);
+});
 
-    // append to div
-    fragment.appendChild(logIn)
+// append to div
+fragment.appendChild(logIn)
 
-    // register Button
-    const register = document.createElement("button");
-    register.type = "button"
-    // Button Text
-    register.textContent = "Register Here"
-    register.onclick = function (event) {
-        console.log(event);
-    };
+// register Button
+const register = document.createElement("button");
+register.type = "button"
+// Button Text
+register.textContent = "Register Here"
+register.onclick = function (event) {
+    console.log(event);
+};
 
-    // append to div
-    fragment.appendChild(register)
-    // append to div
+// append to div
+fragment.appendChild(register)
+// append to div
 
-    const welcomeDiv = document.getElementById("welcomePage");
-    welcomeDiv.appendChild(fragment)
+const welcomeDiv = document.getElementById("welcomePage");
+welcomeDiv.appendChild(fragment)
 
 };
 
